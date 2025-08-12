ca9d71a6cff98d201af792e539ebfef43f770f14
hatim47 <hatimnoor47@gmail.com>
2025-08-12 21:51:41 +0500

    Set Astro base for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
-
+ base: '/astra-project/',
   vite: {    
     plugins: [tailwindcss()],
   },
